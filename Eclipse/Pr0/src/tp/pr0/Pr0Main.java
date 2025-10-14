27e26544bdb4a4295c33a3a54aea91e1098854be
Matrejo <migtrejo@ucm.es>
2025-10-14 11:23:01 +0200

    Created the workspace to open eclipse on

---
+++ Eclipse/Pr0/src/tp/pr0/Pr0Main.java
@@ -0,0 +1,12 @@
+package tp.pr0;
+
+public class Pr0Main {
+
+   public static void writeGreeting(String name) {
+      System.out.println("Hello, " + name);
+   }
+
+   public static void main(String args[]) {
+      Pr0Main.writeGreeting("Bender Bending Rodríguez");
+   }
+}
