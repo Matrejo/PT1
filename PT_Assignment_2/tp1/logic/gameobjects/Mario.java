95eb01f6a69e8d30dae379089579b1d4f9382828
Matrejo <migtrejo@ucm.es>
2025-10-22 16:24:03 +0200

    Add files via upload

---
+++ PT_Assignment_2/tp1/logic/gameobjects/Mario.java
@@ -0,0 +1,13 @@
+package tp1.logic.gameobjects;
+
+public class Mario  {
+
+	//TODO fill your code
+	
+	/**
+	 *  Implements the automatic update	
+	 */
+	public void update() {
+		//TODO fill your code
+	}
+}
